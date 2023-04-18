5edc278bb3ec6086b07306dac4facb117f87421f
Manuel Rodriguez <valle.mrv@gmail.com>
2023-04-18 13:51:51 +0200

    pruebas y testeos

--- django/tpv_server/test_sql.py
+++ django/tpv_server/test_sql.py
@@ -16,14 +16,9 @@ def execute_sql_query(database, query):
         return None
 
 # La consulta SQL que deseas ejecutar
-#query = "SELECT name, sql FROM sqlite_master WHERE type = 'table' AND (name LIKE 'gestion_%' or name NOT LIKE '%\_%' ESCAPE '\\');"
+query = "SELECT name, sql FROM sqlite_master WHERE type = 'table' AND (name LIKE 'gestion_%' or name NOT LIKE '%\_%' ESCAPE '\\');"
 #query = "SELECT * FROM teclas WHERE ID IN (SELECT IDTecla FROM teclaseccion WHERE IDSeccion IN (SELECT ID FROM secciones WHERE Nombre = 'Bocadillos'))"
-query = '''SELECT *
-FROM arqueocaja
-LEFT JOIN gastos ON arqueocaja.ID = gastos.IDArqueo
-LEFT JOIN efectivo ON arqueocaja.ID = efectivo.IDArqueo
-ORDER BY arqueocaja.ID DESC
-;'''
+
 
 # La base de datos donde deseas ejecutar la consulta
 database = "testtpv.sqlite3"
@@ -31,7 +26,12 @@ database = "testtpv.sqlite3"
 results = execute_sql_query(database, query)
 
 if results:
+    result = "{"
     for row in results:
-        print(row)
+        result += "'%s':'%s'," % (row[0], row[1].replace("CREATE TABLE \"%s\" " % row[0],""))
+    
+    result += "}"
+    r = open("structura_info.py", "w")
+    r.write("models="+result)
 else:
     print("La consulta no devuelve resultados.")
