ce36041fd8a72795d20687240615616d39b1ce69
Manuel Rodriguez <valle.mrv@gmail.com>
2023-08-17 03:53:58 +0200

    por fin funciona los camareros

--- android/ValleTPV/ValleTPVLib/src/main/java/com/valleapp/valletpvlib/interfaces/IBaseDatos.kt
+++
@@ -1,9 +0,0 @@
-package com.valleapp.valletpvlib.interfaces
-
-import org.json.JSONArray
-
-interface IBaseDatos {
-    fun filter(cWhere: String?): JSONArray
-    fun rellenarTabla(objs: JSONArray?)
-    fun inicializar()
-}
--- android/ValleTPV/ValleTPVLib/src/main/java/com/valleapp/valletpvlib/interfaces/IBaseSocket.kt
+++
@@ -1,9 +0,0 @@
-package com.valleapp.valletpvlib.interfaces
-
-import org.json.JSONObject
-
-interface IBaseSocket {
-    fun rm(o: JSONObject?)
-    fun insert(o: JSONObject?)
-    fun update(o: JSONObject?)
-}
