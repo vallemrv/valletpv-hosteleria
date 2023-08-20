35bc475f4fb34c13c22e1459da3884151f1d0cfc
Manuel Rodriguez <valle.mrv@gmail.com>
2023-08-20 02:52:50 +0200

    Grid, ValleGrid, ButtonItem

--- android/ValleTPV/ValleTPVLib/src/main/java/com/valleapp/valletpvlib/ui/screens/Camareros.kt
+++ android/ValleTPV/ValleTPVLib/src/main/java/com/valleapp/valletpvlib/ui/screens/Camareros.kt
@@ -4,18 +4,8 @@ import android.app.Application
 import androidx.activity.OnBackPressedCallback
 import androidx.activity.compose.LocalOnBackPressedDispatcherOwner
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.foundation.lazy.grid.items
-import androidx.compose.material3.ExtendedFloatingActionButton
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.getValue
@@ -24,22 +14,17 @@ import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.text.style.TextOverflow
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
-import com.valleapp.valletpvlib.db.Camarero
 import com.valleapp.valletpvlib.db.CamareroDao
+import com.valleapp.valletpvlib.db.IBaseEntity
 import com.valleapp.valletpvlib.models.BindServiceModel
 import com.valleapp.valletpvlib.ui.ToastComposable
+import com.valleapp.valletpvlib.ui.ValleGrid
 import com.valleapp.valletpvlib.ui.ValleTopBar
-import com.valleapp.valletpvlib.ui.theme.Pink00
 
 
 @Composable
@@ -99,16 +84,9 @@ fun CamarerosGrid(
         },
         content = {
             Box(modifier = Modifier.padding(it)) {
-                LazyVerticalGrid(
-                    columns = GridCells.Fixed(5),
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .padding(16.dp),
-                    contentPadding = PaddingValues(4.dp)
-                ) {
-                    items(listaCamareros) { camarero ->
-                        CamareroItem(camarero, navController)
-                    }
+                ValleGrid(columns = 5, botones = listaCamareros ){
+                    val info = (it as IBaseEntity).getInfoField()
+                    navController.navigate("mesas/${info.tag}")
                 }
                 ToastComposable(
                     message = "Presione nuevamente para salir ${3 - count}",
@@ -124,29 +102,3 @@ fun CamarerosGrid(
     )
 }
 
-@Composable
-fun CamareroItem(camarero: Camarero, navController: NavHostController) {
-    ExtendedFloatingActionButton(
-        onClick = {
-            navController.navigate("mesas/${camarero.id}")
-        },
-        modifier = Modifier
-            .padding(8.dp)
-            .height(150.dp),
-        containerColor = Pink00
-
-    ) {
-        Text(
-            text = "${camarero.nombre} ${camarero.apellidos}",
-            maxLines = 3,
-            overflow = TextOverflow.Ellipsis,
-            modifier = Modifier
-                .padding(9.dp)
-                .wrapContentSize(Alignment.Center),
-            lineHeight = 40.sp,
-            color = Color.Black,
-            style = MaterialTheme.typography.labelLarge,
-            textAlign = TextAlign.Center
-        )
-    }
-}
