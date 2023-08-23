9dea53a38ab5277f663c628ca7ee90cb9791fe05
Manuel Rodriguez <valle.mrv@gmail.com>
2023-08-23 19:49:39 +0200

    Generalizacion

--- android/ValleTPV/ValleTPVLib/src/main/java/com/valleapp/valletpvlib/ui/theme/HexToComposeColor.kt
+++
@@ -1,7 +0,0 @@
-package com.valleapp.valletpvlib.ui.theme
-
-import androidx.compose.ui.graphics.Color
-
-fun hexToComposeColor(hex: String): Color {
-    return Color(android.graphics.Color.parseColor(hex))
-}
