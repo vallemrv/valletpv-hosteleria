aa3d84f7b13304c8c66f1a165cf39d187ec7db5a
Manuel Rodriguez <valle.mrv@gmail.com>
2024-06-06 13:23:14 +0200

    Aciones varias esta hace tiempo y no me acuerdo

---
+++ django/valletpv_web/mail_server/constant_config.py
@@ -0,0 +1,6 @@
+SECRET_KEY = '*zt@+1qtnt(488!dh+rxaqsq*g71$v%cg#k*-!9-683z3zvh3f'
+EMAIL_HOST_PASSWORD = 'Calamatraca'
+EMAIL_HOST_USER = 'valleapp.store@gmail.com'
+EMAIL_PORT = '587'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
