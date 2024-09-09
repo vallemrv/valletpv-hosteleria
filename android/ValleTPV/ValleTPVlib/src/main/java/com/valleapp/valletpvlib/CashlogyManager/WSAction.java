b123457fd44e0c122021a24f6219436e744e2525
Manuel Rodriguez <valle.mrv@gmail.com>
2024-09-09 12:47:31 +0200

    websocket para conexion con cashlogy

---
+++ android/ValleTPV/ValleTPVlib/src/main/java/com/valleapp/valletpvlib/CashlogyManager/WSAction.java
@@ -0,0 +1,21 @@
+package com.valleapp.valletpvlib.CashlogyManager;
+
+
+public class InitializeAction extends CashlogyAction {
+
+    public InitializeAction(CashlogySocketManager socketManager) {
+        super(socketManager);
+    }
+
+    @Override
+    public void execute() {
+        // Enviar el comando #I# para inicializar la máquina
+        socketManager.setCurrentAction(this);
+        socketManager.sendCommand("#I#");
+    }
+
+    @Override
+    public void handleResponse(String comando, String response) {
+
+    }
+}
