acb5e1d7c38fd0c6760887123798cdec9f7be9e1
Manuel Rodriguez <valle.mrv@gmail.com>
2024-09-11 10:46:23 +0200

    crear un cashlogy app

---
+++ android/ValleTPV/ValleCASH/src/main/java/com/valleapp/vallecash/ValleCASH.kt
@@ -0,0 +1,47 @@
+package com.valleapp.vallecash
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.valleapp.vallecash.ui.theme.ValleTPVTheme
+
+class ValleCASH : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ValleTPVTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    ValleTPVTheme {
+        Greeting("Android")
+    }
+}
