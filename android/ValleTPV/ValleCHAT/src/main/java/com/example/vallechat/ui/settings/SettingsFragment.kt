db989cd0d549bd79c0c80497fa42358dfc720ccc
Manuel Rodriguez <valle.mrv@gmail.com>
2024-09-27 23:53:26 +0200

    agregar nueva aplicacion chatgpt pedidos

---
+++ android/ValleTPV/ValleCHAT/src/main/java/com/example/vallechat/ui/settings/SettingsFragment.kt
@@ -0,0 +1,34 @@
+package com.example.vallechat.ui.settings
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import com.example.vallechat.R
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [SettingFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class SettingFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_settings, container, false)
+        val tvTitle = view.findViewById<TextView>(R.id.tvTitle)
+        val tvServerAddress = view.findViewById<TextView>(R.id.tvServerAddress)
+
+        tvTitle.text = "Settings"
+        tvServerAddress.text = "Dirección del servidor: http://tu-servidor.com"
+
+        return view
+    }
+}
